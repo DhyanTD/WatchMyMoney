d8a627a04de59f34bd02a674677bf798ba7f9dbc
DhyanTD <dhyan@smoketrees.in>
2024-11-14 22:55:30 +0530

    feat: tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+import { Ionicons } from "@expo/vector-icons";
+import { Stack, Tabs } from "expo-router";
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-    </ThemeProvider>
+    <Tabs
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName = "home" as "home" | "settings" | "list";
+
+          if (route.name === "index") {
+            iconName = "home";
+          } else if (route.name === "accounts/index") {
+            iconName = "settings";
+          }
+          else if (route.name === "records/index") {
+            iconName = "list";
+          }
+
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+      })}
+    >
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="accounts/index"  options={{ title: "Accounts" }} />
+      <Tabs.Screen name="records/index" options={{ title: "Records" }} />
+    </Tabs>
   );
 }
